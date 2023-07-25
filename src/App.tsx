4dbf0131724a0640ac756fee1fe6058f7cbb5109
jbravoh <jbravoharriott@gmail.com>
2023-07-25 21:09:24 +0100

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <></>;
+};
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("no container to render to");
+}
+const root = createRoot(container);
+root.render(<App />);
