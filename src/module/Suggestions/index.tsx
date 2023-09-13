bf168804b6f1c2bc2eef43539ea96d4bd56d72a7
jbravoh <jbravoharriott@gmail.com>
2023-09-13 08:44:54 +0100

    feat: style suggestions section, remove comments in other files

--- src/module/Suggestions/index.tsx
+++ src/module/Suggestions/index.tsx
@@ -3,14 +3,18 @@ import "../../styles/Suggestions.css";
 
 const Suggestions = () => {
   return (
-    <section>
+    <section style={{ marginTop: "6rem" }}>
       <div className="suggestions-wrapper">
         {suggestions.map((suggestion, index) => {
           return (
-            <div key={index}>
+            <div key={index} className="suggestion-holder">
               <img src={suggestion.image} alt="food" />
-              <h3 className="heading-s">{suggestion.label}</h3>
-              <p className="body-s">{suggestion.content}</p>
+              <div>
+                <h3 className="heading-s" style={{ marginBottom: "1.5rem" }}>
+                  {suggestion.label}
+                </h3>
+                <p className="body-s">{suggestion.content}</p>
+              </div>
             </div>
           );
         })}
